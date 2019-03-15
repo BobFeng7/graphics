e9f8e8c7ca5b4bef01602451759f074d48a4e329
Bob Feng <bob.feng.com@gmail.com>
2019-03-14 17:16:48 -0700

    working audio, shitty visualizer

--- main.js
+++ main.js
@@ -1,201 +1,295 @@
+$(document).ready(function () {
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  // |                                                                           | 
+  // |                               Set Up                                      | 
+  // |                                                                           | 
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-// |                                                                           | 
-// |                               Set Up                                      | 
-// |                                                                           | 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// Create Scene, Camera, and Renderer
-var scene = new THREE.Scene( );
-var aspect = window.innerWidth / window.innerHeight;
-var camera = new THREE.PerspectiveCamera( 50, aspect, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer( );
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-// Adjust aspect ratio
-window.addEventListener( 'resize', function ( ) 
-{
+  // Create Scene, Camera, and Renderer
+  var scene = new THREE.Scene( );
+  var aspect = window.innerWidth / window.innerHeight;
+  var camera = new THREE.PerspectiveCamera( 50, aspect, 0.1, 1000 );
+  var renderer = new THREE.WebGLRenderer( );
   renderer.setSize( window.innerWidth, window.innerHeight );
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix( );
-});
+  document.body.appendChild( renderer.domElement );
 
-// Move camera to initial orientation
-camera.position.set( 25, 0, 0 );
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-// |                                                                           | 
-// |                           Shader Stuff                                    | 
-// |                                                                           | 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const gl = renderer.domElement.getContext( "webgl" );
-
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
-var uniforms, material, mesh;
-var mouseX = 0,
-  mouseY = 0,
-  lat = 0,
-  lon = 0,
-  phy = 0,
-  theta = 0;
-
-var aspectRatio = window.innerWidth / window.innerHeight;
-
-uniforms = {
-  time: { type: "f", value: 1.0 },
-  resolution: { type: "v2", value: new THREE.Vector2( ) }
-};
-uniforms.resolution.value.x = window.innerWidth;
-uniforms.resolution.value.y = window.innerHeight;
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-// |                                                                           | 
-// |                           Create Mesh                                     | 
-// |                                                                           | 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-var shaderMaterial = new THREE.ShaderMaterial (
-{
-  uniforms: uniforms,
-  vertexShader: document.getElementById("vertexShader").textContent,
-  fragmentShader: document.getElementById("fragmentShader").textContent,
-  transparent: true,
-  wireframe: true
-});
-shaderSphere = new THREE.Mesh( new THREE.SphereBufferGeometry( 7, 36, 36 ), shaderMaterial );
-scene.add( shaderSphere );
-
-var shaderMaterial2 = new THREE.ShaderMaterial (
-{
-  uniforms: uniforms,
-  vertexShader: document.getElementById("vertexShader2").textContent,
-  fragmentShader: document.getElementById("fragmentShader2").textContent,
-  transparent: true,
-  wireframe: true
-});
-shaderSphere2 = new THREE.Mesh( new THREE.SphereBufferGeometry( 7, 36, 36 ), shaderMaterial2 );
-scene.add( shaderSphere2 );
-
-
-var sphereGeometry = new THREE.SphereGeometry( 5, 30, 30 )
-var sphereGeometry2 = new THREE.SphereGeometry( 5, 30, 30 )
-var sphereMaterial = new THREE.MeshNormalMaterial( { wireframe: true } );
-var sphereMaterial2 = new THREE.MeshNormalMaterial( { wireframe: true } );
-var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-var sphere2 = new THREE.Mesh( sphereGeometry2, sphereMaterial2 );
-var sphere3 = new THREE.Mesh( sphereGeometry2, sphereMaterial2 );
-scene.add( sphere );
-scene.add( sphere2 );
-scene.add( sphere3 );
-
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-// |                                                                           | 
-// |                           Helper Functions                                | 
-// |                                                                           | 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// set interval function structure
-var setTimer = function ( time ) 
-{
-  var timer = setInterval( function ( ) 
+  // Adjust aspect ratio
+  window.addEventListener( 'resize', function ( ) 
   {
-    // Do something here
-  }, time);
-};
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix( );
+  });
+
+  // Move camera to initial orientation
+  camera.position.set( 25, 0, 0 );
+
+  controls = new THREE.OrbitControls( camera, renderer.domElement );
 
 
-var clockScaler = function ( obj ) 
-{
-  var t = uniforms.time.value/2;
-  obj.scale.set( Math.abs( Math.cos( t ) ), Math.abs( Math.cos( t ) ), Math.abs( Math.cos( t ) ) );
-};
 
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  // |                                                                           | 
+  // |                               Audio Stuff                                 | 
+  // |                                                                           | 
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //get the audio context
+  var audioContext = new AudioContext();
 
-var invClockScaler = function ( obj ) 
-{
-  var t = uniforms.time.value/2;
-  obj.scale.set( 1/Math.abs( Math.cos( t ) ), 1/Math.abs( Math.cos( t ) ), 1/Math.abs( Math.cos( t ) ) );
-  // NOTE: using tan for all three creates a 0 to inf scaling obj
-};
+  //create the javascript node
+  var javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+  javascriptNode.connect(audioContext.destination);
 
+  //create the source buffer
+  var sourceBuffer = audioContext.createBufferSource();
 
-var clockRotatorSin = function ( obj ) 
-{
-  var t = uniforms.time.value/2;
-  obj.rotation.set( Math.sin( t ), Math.cos( t ), Math.tan( t ) );
-};
+  //create the analyser node
+  var analyser = audioContext.createAnalyser();
+  analyser.smoothingTimeConstant = 0.3;
+  analyser.fftSize = 512;
 
+  //connect source to analyser
+  sourceBuffer.connect( analyser );
 
-var clockRotatorCos = function ( obj ) 
-{
-  var t = uniforms.time.value/2;
-  obj.rotation.set( Math.cos( t ), Math.tan( t ), Math.sin( t ) );
-}
+  //analyser to speakers
+  analyser.connect( javascriptNode );
 
-var clockRotatorTan = function ( obj ) 
-{
-  var t = uniforms.time.value/2;
-  obj.rotation.set( Math.tan( t ), Math.sin( t ), Math.cos( t ) );
-}
+  //connect source to analyser
+  sourceBuffer.connect( audioContext.destination );
 
+  //this is where we animates the bars
+  javascriptNode.onaudioprocess = function () {
 
-var rotator = function ( obj ) 
-{
-  obj.rotation.x += 0.005;
-  obj.rotation.y += 0.005;
-  obj.rotation.z += 0.005;
-}
+      // get the average for the first channel
+      var array = new Uint8Array( analyser.frequencyBinCount);
+      analyser.getByteFrequencyData(array);
 
+      //render the scene and update controls
+      renderer.render( scene, camera );
+      controls.update( );
 
-var sphereHandler = function ( ) 
-{
-  clockRotatorSin( sphere );
-  clockScaler( sphere );
+      // var step = Math.round(array.length / visualizer.numberOfBars);
+
+      //Iterate through the bars and scale the z axis
+      // for (var i = 0; i < visualizer.numberOfBars; i++) {
+      //     var value = array[i * step] / 4;
+      //     value = value < 1 ? 1 : value;
+      //     visualizer.bars[i].scale.z = value;
+      // }
+      var value = array[0];
+      console.log(value);
+      sphere.scale.x = value/150;
+      sphere.scale.y = value/150;
+      sphere.scale.z = value/150;
+  }
+  var start = function ( buffer ) 
+  {
+    audioContext.decodeAudioData(buffer, decodeAudioDataSuccess, decodeAudioDataFailed);
+
+    function decodeAudioDataSuccess(decodedBuffer) {
+        sourceBuffer.buffer = decodedBuffer
+        sourceBuffer.start(0);
+    }
+
+    function decodeAudioDataFailed() {
+        debugger
+    }
+  }
   
-  clockRotatorCos( sphere2 );
-  invClockScaler( sphere2 );
 
-  clockRotatorTan( sphere3 );
-}
+  //drag Enter
+  document.body.addEventListener("dragenter", function () {
+     
+  }, false);
+
+  //drag over
+  document.body.addEventListener("dragover", function (e) {
+      e.stopPropagation();
+      e.preventDefault();
+      e.dataTransfer.dropEffect = 'copy';
+  }, false);
+
+  //drag leave
+  document.body.addEventListener("dragleave", function () {
+     
+  }, false);
+
+  //drop
+  document.body.addEventListener("drop", function (e) {
+      e.stopPropagation();
+
+      e.preventDefault();
+
+      //get the file
+      var file = e.dataTransfer.files[0];
+      var fileName = file.name;
+
+      $("#guide").text("Playing " + fileName);
+
+      var fileReader = new FileReader();
+
+      fileReader.onload = function (e) {
+          var fileResult = e.target.result;
+          start(fileResult);
+      };
+
+      fileReader.onerror = function (e) {
+        debugger
+      };
+     
+      fileReader.readAsArrayBuffer(file);
+  }, false);
 
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-// |                                                                           | 
-// |                           Animation Loops                                 | 
-// |                                                                           | 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  // |                                                                           | 
+  // |                           Shader Stuff                                    | 
+  // |                                                                           | 
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  var uniforms = {
+    time: { type: "f", value: 1.0 },
+    resolution: { type: "v2", value: new THREE.Vector2( ) }
+  };
+  uniforms.resolution.value.x = window.innerWidth;
+  uniforms.resolution.value.y = window.innerHeight;
 
-// Called every frame at anything you're checking, if you're moving something etc.
-var update = function ( ) 
-{
-  var elapsedMilliseconds = Date.now() - startTime;
-  var elapsedSeconds = elapsedMilliseconds / 1000;
-  uniforms.time.value = elapsedSeconds / 2;
-  sphereHandler( );
-  // meshClockRotator( torusKnot1 );
-};
 
-// Called every frame to draw the scene
-var render = function ( ) 
-{
-  
-  renderer.render( scene, camera );
-};
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  // |                                                                           | 
+  // |                           Create Mesh                                     | 
+  // |                                                                           | 
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  var shaderMaterial = new THREE.ShaderMaterial (
+  {
+    uniforms: uniforms,
+    vertexShader: document.getElementById("vertexShader").textContent,
+    fragmentShader: document.getElementById("fragmentShader").textContent,
+    transparent: true,
+    wireframe: true
+  });
+  shaderSphere = new THREE.Mesh( new THREE.SphereBufferGeometry( 7, 36, 36 ), shaderMaterial );
+  scene.add( shaderSphere );
 
-// called every animation frame as the master game loop, calls UPDATE and RENDER
-var Animate = function ( ) 
-{
-  requestAnimationFrame( Animate );
-  update( );
-  render( );
-};
+  var shaderMaterial2 = new THREE.ShaderMaterial (
+  {
+    uniforms: uniforms,
+    vertexShader: document.getElementById("vertexShader2").textContent,
+    fragmentShader: document.getElementById("fragmentShader2").textContent,
+    transparent: true,
+    wireframe: true
+  });
+  shaderSphere2 = new THREE.Mesh( new THREE.SphereBufferGeometry( 7, 36, 36 ), shaderMaterial2 );
+  scene.add( shaderSphere2 );
 
-var startTime = Date.now();
-Animate( );
+
+  var sphereGeometry = new THREE.SphereGeometry( 5, 30, 30 )
+  var sphereGeometry2 = new THREE.SphereGeometry( 5, 30, 30 )
+  var sphereMaterial = new THREE.MeshNormalMaterial( { wireframe: true } );
+  var sphereMaterial2 = new THREE.MeshNormalMaterial( { wireframe: true } );
+  var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+  var sphere2 = new THREE.Mesh( sphereGeometry2, sphereMaterial2 );
+  var sphere3 = new THREE.Mesh( sphereGeometry2, sphereMaterial2 );
+  scene.add( sphere );
+  scene.add( sphere2 );
+  scene.add( sphere3 );
+
+
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  // |                                                                           | 
+  // |                           Helper Functions                                | 
+  // |                                                                           | 
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  var randomColor = function ( ) 
+  {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  var clockScaler = function ( obj ) 
+  {
+    var t = uniforms.time.value/2;
+    obj.scale.set( Math.abs( Math.cos( t ) ), Math.abs( Math.cos( t ) ), Math.abs( Math.cos( t ) ) );
+  };
+
+  var invClockScaler = function ( obj ) 
+  {
+    var t = uniforms.time.value/2;
+    obj.scale.set( 1/Math.abs( Math.cos( t ) ), 1/Math.abs( Math.cos( t ) ), 1/Math.abs( Math.cos( t ) ) );
+    // NOTE: using tan for all three creates a 0 to inf scaling obj
+  };
+
+  var clockRotatorSin = function ( obj ) 
+  {
+    var t = uniforms.time.value/2;
+    obj.rotation.set( Math.sin( t ), Math.cos( t ), Math.tan( t ) );
+  };
+
+  var clockRotatorCos = function ( obj ) 
+  {
+    var t = uniforms.time.value/2;
+    obj.rotation.set( Math.cos( t ), Math.tan( t ), Math.sin( t ) );
+  }
+
+  var clockRotatorTan = function ( obj ) 
+  {
+    var t = uniforms.time.value/2;
+    obj.rotation.set( Math.tan( t ), Math.sin( t ), Math.cos( t ) );
+  }
+
+  var rotator = function ( obj ) 
+  {
+    obj.rotation.x += 0.005;
+    obj.rotation.y += 0.005;
+    obj.rotation.z += 0.005;
+  }
+
+  var sphereHandler = function ( ) 
+  {
+    // clockRotatorSin( sphere );
+    // clockScaler( sphere );
+    
+    clockRotatorCos( sphere2 );
+    invClockScaler( sphere2 );
+
+    clockRotatorTan( sphere3 );
+  }
+
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  // |                                                                           | 
+  // |                           Animation Loops                                 | 
+  // |                                                                           | 
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  // Called every frame at anything you're checking, if you're moving something etc.
+  var update = function ( ) 
+  {
+    var elapsedMilliseconds = Date.now() - startTime;
+    var elapsedSeconds = elapsedMilliseconds / 1000;
+    uniforms.time.value = elapsedSeconds / 2;
+    sphereHandler( );
+  };
+
+  // Called every frame to draw the scene
+  var render = function ( ) 
+  {
+    
+    renderer.render( scene, camera );
+  };
+
+  // called every animation frame as the master game loop, calls UPDATE and RENDER
+  var Animate = function ( ) 
+  {
+    requestAnimationFrame( Animate );
+    update( );
+    render( );
+  };
+
+  var startTime = Date.now();
+  Animate( );
+});

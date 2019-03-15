99cc62eb13ad9527315f718352746f930a61f877
Bob Feng <bob.feng.com@gmail.com>
2019-03-15 16:51:24 -0700

    Testing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,62 @@
 $(document).ready(function () {
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  // |                                                                           | 
+  // |                           Helper Functions                                | 
+  // |                                                                           | 
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  var range = function (start, end) {
+    return (new Array(end - start + 1)).fill(undefined).map((_, i) => i + start);
+  };
+
+  var randomColor = function ( ) 
+  {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  var clockScaler = function ( obj ) 
+  {
+    var t = uniforms.time.value/2;
+    obj.scale.set( Math.abs( Math.cos( t ) ), Math.abs( Math.cos( t ) ), Math.abs( Math.cos( t ) ) );
+  };
+
+  var invClockScaler = function ( obj ) 
+  {
+    var t = uniforms.time.value/2;
+    obj.scale.set( 1/Math.abs( Math.cos( t ) ), 1/Math.abs( Math.cos( t ) ), 1/Math.abs( Math.cos( t ) ) );
+    // NOTE: using tan for all three creates a 0 to inf scaling obj
+  };
+
+  var clockRotatorSin = function ( obj ) 
+  {
+    var t = uniforms.time.value/2;
+    obj.rotation.set( Math.sin( t ), Math.cos( t ), Math.tan( t ) );
+  };
+
+  var clockRotatorCos = function ( obj ) 
+  {
+    var t = uniforms.time.value/2;
+    obj.rotation.set( Math.cos( t ), Math.tan( t ), Math.sin( t ) );
+  }
+
+  var clockRotatorTan = function ( obj ) 
+  {
+    var t = uniforms.time.value/2;
+    obj.rotation.set( Math.tan( t ), Math.sin( t ), Math.cos( t ) );
+  }
+
+  var rotator = function ( obj ) 
+  {
+    obj.rotation.x += 0.005;
+    obj.rotation.y += 0.005;
+    obj.rotation.z += 0.005;
+  }
+
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
   // |                                                                           | 
   // |                               Set Up                                      | 
@@ -22,7 +80,7 @@ $(document).ready(function () {
   });
 
   // Move camera to initial orientation
-  camera.position.set( 25, 0, 0 );
+  camera.position.set( 0, 25, 0 );
 
   controls = new THREE.OrbitControls( camera, renderer.domElement );
 
@@ -61,26 +119,57 @@ $(document).ready(function () {
   javascriptNode.onaudioprocess = function () {
 
       // get the average for the first channel
-      var array = new Uint8Array( analyser.frequencyBinCount);
-      analyser.getByteFrequencyData(array);
+      var array = new Uint8Array( analyser.frequencyBinCount );
+      analyser.getByteFrequencyData( array );
 
       //render the scene and update controls
       renderer.render( scene, camera );
       controls.update( );
 
-      // var step = Math.round(array.length / visualizer.numberOfBars);
+      // console.log(array);
+      // console.log(array.length);
 
-      //Iterate through the bars and scale the z axis
-      // for (var i = 0; i < visualizer.numberOfBars; i++) {
-      //     var value = array[i * step] / 4;
-      //     value = value < 1 ? 1 : value;
-      //     visualizer.bars[i].scale.z = value;
-      // }
-      var value = array[0];
-      console.log(value);
-      sphere.scale.x = value/150;
-      sphere.scale.y = value/150;
-      sphere.scale.z = value/150;
+      var x = 16;
+      var step = Math.round( array.length / x );
+
+      // Float32Array(4107) [5, 5, 5, -0, 5, 0, -0, ... Every three items defines a new point
+      sphereP = sphere.geometry.attributes.position;
+      sphereV = sphereP.array;
+      var V_step = Math.round ( sphereV.length / x );
+
+      console.log( "~~~~~~~~~~~~" );
+      console.log( sphereV.length );
+      // sphereV has d  objects 
+      // array has 256 objects
+      // iterate through 16 times total
+
+      for ( var i = 0; i < x; i++ ) 
+      {
+          var value = array[i];
+          // value = i * 2;
+          console.log( value );
+          value = (value) / 100;
+          // console.log( value );
+          
+          for ( var j = 0; j < V_step; j++ ) 
+          {
+            if (value != 0) 
+            {
+              sphereV[i+j] *= value;
+            }
+          }
+      }
+      sphereP.setArray( sphereV )
+      sphereP.needsUpdate = true;
+      console.log( sphereP.array );
+
+      
+
+      // var value = array[0];
+      // // console.log(value);
+      // sphere.scale.x = 1 + value/1500;
+      // sphere.scale.y = 1 + value/1500;
+      // sphere.scale.z = 1 + value/1500;
   }
   var start = function ( buffer ) 
   {
@@ -96,6 +185,13 @@ $(document).ready(function () {
     }
   }
   
+
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  // |                                                                           | 
+  // |                           Loading Mp3 Audio                               | 
+  // |                                                                           | 
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   //drag Enter
   document.body.addEventListener("dragenter", function () {
@@ -182,75 +278,30 @@ $(document).ready(function () {
   scene.add( shaderSphere2 );
 
 
-  var sphereGeometry = new THREE.SphereGeometry( 5, 30, 30 )
-  var sphereGeometry2 = new THREE.SphereGeometry( 5, 30, 30 )
+  var sphereGeometry = new THREE.SphereBufferGeometry( 5, 36, 36 )
+  var sphereGeometry2 = new THREE.SphereBufferGeometry( 5, 36, 36 )
   var sphereMaterial = new THREE.MeshNormalMaterial( { wireframe: true } );
   var sphereMaterial2 = new THREE.MeshNormalMaterial( { wireframe: true } );
   var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
   var sphere2 = new THREE.Mesh( sphereGeometry2, sphereMaterial2 );
   var sphere3 = new THREE.Mesh( sphereGeometry2, sphereMaterial2 );
   scene.add( sphere );
-  scene.add( sphere2 );
-  scene.add( sphere3 );
+  // scene.add( sphere2 );
+  // scene.add( sphere3 );
 
+  
+
+  
 
 
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
   // |                                                                           | 
-  // |                           Helper Functions                                | 
+  // |                           Animation Loops                                 | 
   // |                                                                           | 
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  var randomColor = function ( ) 
-  {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
-  var clockScaler = function ( obj ) 
-  {
-    var t = uniforms.time.value/2;
-    obj.scale.set( Math.abs( Math.cos( t ) ), Math.abs( Math.cos( t ) ), Math.abs( Math.cos( t ) ) );
-  };
-
-  var invClockScaler = function ( obj ) 
-  {
-    var t = uniforms.time.value/2;
-    obj.scale.set( 1/Math.abs( Math.cos( t ) ), 1/Math.abs( Math.cos( t ) ), 1/Math.abs( Math.cos( t ) ) );
-    // NOTE: using tan for all three creates a 0 to inf scaling obj
-  };
-
-  var clockRotatorSin = function ( obj ) 
-  {
-    var t = uniforms.time.value/2;
-    obj.rotation.set( Math.sin( t ), Math.cos( t ), Math.tan( t ) );
-  };
-
-  var clockRotatorCos = function ( obj ) 
-  {
-    var t = uniforms.time.value/2;
-    obj.rotation.set( Math.cos( t ), Math.tan( t ), Math.sin( t ) );
-  }
-
-  var clockRotatorTan = function ( obj ) 
-  {
-    var t = uniforms.time.value/2;
-    obj.rotation.set( Math.tan( t ), Math.sin( t ), Math.cos( t ) );
-  }
-
-  var rotator = function ( obj ) 
-  {
-    obj.rotation.x += 0.005;
-    obj.rotation.y += 0.005;
-    obj.rotation.z += 0.005;
-  }
-
   var sphereHandler = function ( ) 
   {
-    // clockRotatorSin( sphere );
+    clockRotatorSin( sphere );
     // clockScaler( sphere );
     
     clockRotatorCos( sphere2 );
@@ -259,20 +310,13 @@ $(document).ready(function () {
     clockRotatorTan( sphere3 );
   }
 
-
-  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-  // |                                                                           | 
-  // |                           Animation Loops                                 | 
-  // |                                                                           | 
-  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
   // Called every frame at anything you're checking, if you're moving something etc.
   var update = function ( ) 
   {
     var elapsedMilliseconds = Date.now() - startTime;
     var elapsedSeconds = elapsedMilliseconds / 1000;
     uniforms.time.value = elapsedSeconds / 2;
-    sphereHandler( );
+    // sphereHandler( );
   };
 
   // Called every frame to draw the scene
